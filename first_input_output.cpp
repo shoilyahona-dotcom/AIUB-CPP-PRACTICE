4169877e87ad9300c57845ea75b8d227bdde5565
shoilyahona-dotcom <shoily.ahona@gmail.com>
2026-07-05 13:16:04 +0600

    Implement basic input and output for two numbers
    Fix output formatting for sum display.

---
+++ first_input_output.cpp
@@ -0,0 +1,16 @@
+
+#include <iostream>
+
+int main()
+{
+    std::cout<<"Enter two numbers:"<<std::endl;
+    int n1=0;
+    int n2=0;
+    std::cin>>n1>>n2;
+    std::cout<<"the sum of "<<n1<<" and "<<n2<<" is "<<n1+n2<<std::endl;
+
+
+
+
+    return 0;
+}
