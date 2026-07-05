345dc674a01db317c8b0c64a333b9407a6a1df28
shoilyahona-dotcom <shoily.ahona@gmail.com>
2026-07-05 13:10:45 +0600

    Add first arithmetic expression program
    Prints results of various arithmetic operations.

---
+++ first_arithmetic_expression.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    cout << 5 + 3<<endl;
+    cout << 10 - 4<<endl;
+    cout << 6 * 5<<endl;
+    cout << 20 / 4<<endl;
+    cout << 17 % 5<<endl;
+    //8, 6, 30, 5, 2
+    cout << (4 + 3) * 2<<endl;
+     cout << 18 / 3 + 2;
+     //14, 8
+    return 0;
+}
